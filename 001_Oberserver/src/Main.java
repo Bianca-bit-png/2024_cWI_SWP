eaf0d245b67125caa0f2616fa4edcc6b405535b5
Bianca-bit-png <bianca.hosp@student.htldornbirn.at>
2024-10-07 16:26:58 +0200

    fertiggestellt Observer-Part2

--- 001_Oberserver/src/Main.java
+++ 001_Oberserver/src/Main.java
@@ -1,13 +1,28 @@
-import at.htldornbirn_4cwi.swp.oberserver.NewsAgency;
+import com.example.newsbroker.NewsAgency;
+import com.example.newsbroker.NewsChannel;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        //instantiate the news Agencys
+        var apa = new NewsAgency("APA");
+        var reuters = new NewsAgency("Reuters");
 
-        var newsAgency = new NewsAgency("APA");
+        //instantiate the news channels
+        var orf = new NewsChannel("orf.at");
+        var nzz = new NewsChannel("nzz.ch");
+        var nytimes = new NewsChannel("nytimes.com");
 
-        System.out.println(newsAgency);
-    }
+        //subscribe to APA
+        apa.subscribe(orf);
+        apa.subscribe(nzz);
+
+        //subscribe to Reuters
+        reuters.subscribe(nytimes);
+        reuters.subscribe(nzz);
+
+        //broadcast news
+        apa.broadcast("Auf der Suche nach der nächsten Registrierung");
+        reuters.broadcast("Harrus campaign raises $55 mln over two weekend eventsy, campaign official says");
+}
 
 }
